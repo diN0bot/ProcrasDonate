9979f9c3558694fa6b8998f874909e44ee020ca3
diN0bot <diN0bot@whynoti.org>
2009-05-04 21:18:25 -0400

    page menu knows which page is selected. menu, logo and footer futzed.

--- lib/context.py
+++ lib/context.py
@@ -4,7 +4,7 @@ def defaults(request):
     frame = {}
     frame.update(literals())
     frame.update(django_server())
-    print request.path
+    frame.update(selected(request))
     return frame
 
 def literals():
@@ -14,3 +14,9 @@ def literals():
 
 def django_server():
     return { 'DJANGO_SERVER': settings.DJANGO_SERVER }
+
+def selected(request):
+    # u"/foo/" gets split into [u"", u"foo", u""]
+    # '/' is appended to all URLs, regardless of what user requests
+    # is this robust?! we'll see come deploy time...
+    return { 'selected': request.path.split('/')[-2] }
