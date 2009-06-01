3299bf68ffd910bcdf5a2755678ad35f08cff49d
cwallardo <clay@bilumi.org>
2009-06-01 14:17:53 -0400

    lucy did some .js and env things that are not tacked onto this commit.
    finished changing main.js for recipients.  changed the one that said (not updated) but not the one that said (ignored).
    
    changed recipient_snippet.html to remove test.
    
    added more images.  changed some existing.
    
    filled in home.html (moved several items from learn_more page.)
    
    changed column margin on small images to get columns lining up nice.
    
    made screenshots and added them to home page.
    
    gonna take bear for a walk.

--- procrasdonate/ff_extn/ProcrasDonate/content/templates/unsorted_wrap.html.js
+++ procrasdonate/ff_extn/ProcrasDonate/content/templates/unsorted_wrap.html.js
@@ -1,3 +1,3 @@
 
-    Template.compile(["<span class='img_link move_to_procrasdonate'>\n\t<img class='Move_Site_Arrow' src='", ["var", ["constants", "MEDIA_URL"], []], "img\/LeftArrow.png'>\n<\/span>\n", ["var", ["inner"], []], "\n<span class='img_link move_to_timewellspent'>\n\t<img class='Move_Site_Arrow' src='", ["var", ["constants", "MEDIA_URL"], []], "img\/RightArrow.png'>\n<\/span>\n"], "unsorted_wrap");
+    Template.compile(["<span class='img_link move_to_procrasdonate'>\n\t<img class='Move_Site_Arrow' src='", ["var", ["constants", "MEDIA_URL"], []], "img/LeftArrow.png'>\n</span>\n", ["var", ["inner"], []], "\n<span class='img_link move_to_timewellspent'>\n\t<img class='Move_Site_Arrow' src='", ["var", ["constants", "MEDIA_URL"], []], "img/RightArrow.png'>\n</span>\n"], "unsorted_wrap");
     
