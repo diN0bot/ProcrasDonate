b6b30baf77bc9b9ad97e1b989dd36d498e65f611
cwallardo <clay@bilumi.org>
2009-06-16 14:56:32 -0400

    next_prev_buttons template added so, registration not broken.

---
+++ procrasdonate/ff_extn/ProcrasDonate/content/templates/next_prev_buttons.html.js
@@ -0,0 +1,3 @@
+
+    Template.compile(["<table>\n\t<tbody>\n\t\t<tr>\n\t\t\t<td><img src=\"", ["var", ["MEDIA_URL"], []], "img/NextArrow.png\">\n\t\t\t</td>\n\t\t\t<td><span class=\"NextPrevSpacer\"></span></td>\n\t\t\t<td><img src=\"", ["var", ["MEDIA_URL"], []], "img/NextArrow.png\">\n\t\t\t</td>\n\t\t</tr>\n\t</tbody>\n</table>\t\t\t\t"], "next_prev_buttons");
+    
