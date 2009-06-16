f61490ba3b87e36c29cacab9e3f1091344e56676
cwallardo <clay@bilumi.org>
2009-06-16 15:56:01 -0400

    Made css for buttons including recipients page.

--- procrasdonate/ff_extn/ProcrasDonate/content/templates/recipients_middle.html.js
+++ procrasdonate/ff_extn/ProcrasDonate/content/templates/recipients_middle.html.js
@@ -1,3 +1,3 @@
 
-    Template.compile(["<h3 class='RegisterHeader'>Your charitable recipients.<\/h3>\n<p><ul>\n\t<li>Add <b>non-profits<\/b> from the list at the bottom.<\/li>\n\t<li>Assign a <b>percentage<\/b> of your pledges to each selection.<\/li>\n<\/ul><\/p>\n<div id='user_recipients'>\n\t", ["var", ["user_recipients"], []], "\n<\/div>\n<div id='potential_recipients'>\n\t", ["var", ["potential_recipients"], []], "\n<\/div>\n\n"], "recipients_middle");
+    Template.compile(["<h3 class='RegisterHeader'>Your charitable recipients.</h3>\n<p><ul>\n\t<li>Add <b>non-profits</b> from the list at the bottom.</li>\n\t<li>Assign a <b>percentage</b> of your pledges to each selection.</li>\n</ul></p>\n<div id='user_recipients'>\n\t", ["var", ["user_recipients"], []], "\n</div>\n<div id='potential_recipients'>\n\t", ["var", ["potential_recipients"], []], "\n</div>\n\n"], "recipients_middle");
     
