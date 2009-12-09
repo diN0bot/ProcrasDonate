eb15381ff5257d17b094ea8ffec47e3d02b2abf5
diN0bot <diN0bot@bilumi.org>
2009-12-09 10:11:22 -0500

    rolled over to 0.3.4

---
+++ procrasdonate/ProcrasDonateFFExtn/content/templates/user_messages.html.js
@@ -0,0 +1,3 @@
+
+    Template.compile(["<table id=\"user_messages\">\n<tbody><tr><td>\n\t<ul>\n    \t<li class=\"message\">", ["var", ["message"], []], "</li>\n\t</ul>\n</td></tr></tbody>\n</table>\n"], "user_messages");
+    
