8739fce4217fb9e236502e08d6d2156ca2fe92be
diN0bot <diN0bot@whynoti.org>
2010-02-18 10:17:16 -0500

    trying to get very old repo on old computer up to date with current repo...hold on a sec...

--- procrasdonate/ProcrasDonateFFExtn/content/templates/register_done_middle.html.js
+++ procrasdonate/ProcrasDonateFFExtn/content/templates/register_done_middle.html.js
@@ -1,3 +1,3 @@
 
-    Template.compile(["<h2>Registration Success!</h2>\n\n<p>Use <a href=\"", ["var", ["constants", "SETTINGS_URL"], []], "\">My Settings</a> from the menu to the left to modify your settings.</p>\n"], "register_done_middle");
+    Template.compile(["<h2>Registration Success!<\/h2>\n\n<p>Use <a href=\"", ["var", ["constants", "SETTINGS_URL"], []], "\">My Settings<\/a> from the menu to the left to modify your settings.<\/p>\n"], "register_done_middle");
     
--- procrasdonate/ProcrasDonateFFExtn/content/templates/timewellspent_wrap.html.js
+++ procrasdonate/ProcrasDonateFFExtn/content/templates/timewellspent_wrap.html.js
@@ -1,3 +1,3 @@
 
-    Template.compile(["<span class='img_link move_to_unsorted'>\n\t<img class='Move_Site_Arrow' src='", ["var", ["constants", "MEDIA_URL"], []], "img/LeftArrow.png'>\n</span>\n", ["var", ["inner"], []], "\n"], "timewellspent_wrap");
+    Template.compile(["<span class='img_link move_to_unsorted'>\n\t<img class='Move_Site_Arrow' src='", ["var", ["constants", "MEDIA_URL"], []], "img\/LeftArrow.png'>\n<\/span>\n", ["var", ["inner"], []], "\n"], "timewellspent_wrap");
     
